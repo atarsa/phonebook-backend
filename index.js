2cb80492c6d737aded064ac80d6e5e2b268ec034
ATarsa <agnieszkatarsa@gmail.com>
2019-10-14 17:03:48 +0100

    Exercise 3.4 Implement delete person

--- index.js
+++ index.js
@@ -41,6 +41,7 @@ app.get('/api/persons/:id', (req, res) => {
   }
 })
 
+
 app.get('/info', (req,res) => {
   
   res.send(`
@@ -50,6 +51,13 @@ app.get('/info', (req,res) => {
       ${new Date()}
     </div>
   `)
+})
+
+app.delete('/api/persons/:id', (req,res) => {
+  const id = Number(req.params.id)
+  persons = persons.filter(person => person.id !== id)
+
+  res.status(204).end()
 })
 
 
