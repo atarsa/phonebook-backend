c2ddabffb35dde852de3f4d4c2a3ab257c08fbdf
ATarsa <agnieszkatarsa@gmail.com>
2019-10-14 17:29:05 +0100

    Exercise 3.5 Implement add person

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
-
 const app = express()
+
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
 
 let persons = [
   
@@ -25,6 +28,8 @@ let persons = [
       "id": 4
     }  
 ]
+// generate random id
+const generateId = () => Math.floor(Math.random() * Math.floor(10000))
 
 app.get('/api/persons', (req, res) => {
   res.json(persons)
@@ -41,9 +46,41 @@ app.get('/api/persons/:id', (req, res) => {
   }
 })
 
+app.delete('/api/persons/:id', (req,res) => {
+  const id = Number(req.params.id)
+  persons = persons.filter(person => person.id !== id)
+
+  res.status(204).end()
+})
+
+app.post('/api/persons', (req, res) => {
+  const body = req.body
+
+  if (!body.name){
+    return res.status(400).json({
+      error: 'Name missing'
+    })
+  }
+
+  if (!body.number){
+    return res.status(400).json({
+      error: 'Number missing'
+    })
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId()
+  }
+
+  persons = persons.concat(person)
+
+  res.json(person)
+
+})
 
 app.get('/info', (req,res) => {
-  
   res.send(`
     <div>
       Phonebook has info for ${persons.length} people
@@ -53,12 +90,7 @@ app.get('/info', (req,res) => {
   `)
 })
 
-app.delete('/api/persons/:id', (req,res) => {
-  const id = Number(req.params.id)
-  persons = persons.filter(person => person.id !== id)
 
-  res.status(204).end()
-})
 
 
 const PORT = 3001
