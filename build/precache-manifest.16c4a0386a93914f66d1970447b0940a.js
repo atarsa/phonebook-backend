f8ff09af904a5899d5c864e90c48a4286dae696d
ATarsa <agnieszkatarsa@gmail.com>
2019-10-15 19:58:07 +0100

    Build and deploy

---
+++ build/precache-manifest.16c4a0386a93914f66d1970447b0940a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be279aa80911f9c66b1ff2bcd1007bdf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f9a6ba887d6f8f51677a",
+    "url": "/static/css/main.9b9cc5fb.chunk.css"
+  },
+  {
+    "revision": "40e385085b54b414b318",
+    "url": "/static/js/2.de087970.chunk.js"
+  },
+  {
+    "revision": "f9a6ba887d6f8f51677a",
+    "url": "/static/js/main.ddacbcb0.chunk.js"
+  },
+  {
+    "revision": "d92e7acfcbfa197868a5",
+    "url": "/static/js/runtime-main.d36c9b34.js"
+  }
+]);
--- index.js
+++ index.js
@@ -10,6 +10,7 @@ morgan.token('body', function (req, res) {
 
 app.use(bodyParser.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+app.use(express.static('build'))
 
 let persons = [
   
@@ -111,7 +112,7 @@ const unknownEndpoint = (req,res) => {
 
 app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
